a36a8c0a98cbadaed765e54ccd82e36d139526a2
Lukas Martinelli <me@lukasmartinelli.ch>
2015-03-26 20:59:18 +0100

    Switched to newest redis-pipe and use PYTHONUNBUFFERED

--- redis-worker.sh
+++ redis-worker.sh
@@ -2,5 +2,6 @@
 INPUT_LIST=${INPUT_LIST:-repostruct:repos}
 OUTPUT_LIST=${OUTPUT_LIST:-repostruct:results}
 BATCH_SIZE=${BATCH_SIZE:-40}
+PYTHONUNBUFFERED="true"
 
 ./redis-pipe --count $BATCH_SIZE $INPUT_LIST | ./repostruct.py | ./redis-pipe $OUTPUT_LIST
