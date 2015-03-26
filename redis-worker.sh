6986d6b9d21ce792489efe37203db56a3cb01939
Lukas Martinelli <me@lukasmartinelli.ch>
2015-03-26 11:15:50 +0100

    Fixed redis-worker

--- redis-worker.sh
+++ redis-worker.sh
@@ -2,4 +2,4 @@
 LIST_NAME=${LIST_NAME:-repostruct:repos}
 BATCH_SIZE=${BATCH_SIZE:-40}
 
-redis-pipe --count $BATCH_SIZE $LIST_NAME | python repostruct.py | redis-pipe $LIST_NAME
+./redis-pipe --count $BATCH_SIZE $LIST_NAME | ./repostruct.py | ./redis-pipe $LIST_NAME
