1b8cf9fe845702c58aee70241b42d60d0cdcea45
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-08 16:05:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+requires = [
+    'docopt==0.6.2',
+    'requests==2.6.0',
+    'python3-pika==0.9.14',
+]
+
+setup(
+    name='repostruct',
+    version='0.1',
+    url='http://github.com/lukasmartinelli/repostruct/',
+    author="Lukas Martinelli",
+    author_email="me@lukasmartinelli.ch",
+    packages=['repostruct'],
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False,
+    scripts = ['repostruct/repostruct.py']
+)
