c9a7f88c243282a3032eb81918f7d8f2a895dff8
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-08 14:31:55 +0200

    Moved code into module

---
+++ repostruct/last-github-repos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Iterate through all repositories from GitHub
+that are not forks.
+"""
+import sys
+import time
+import os
+
+from github3 import login
+
+
+GITHUB_ACCESS_TOKEN = os.getenv('GITHUB_ACCESS_TOKEN')
+LAST_REPO_ID = int(os.getenv('LAST_REPO_ID', '-1'))
+
+if __name__ == '__main__':
+    gh = login(token=GITHUB_ACCESS_TOKEN)
+    for repo in gh.iter_all_repos(per_page=200, since=LAST_REPO_ID):
+        #import ipdb; ipdb.set_trace()
+        if not repo.fork:
+            print(repo.id, repo.full_name)
